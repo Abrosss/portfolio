0962ab9f3417be300435fc8f47854acd1fc4c933
Abrosss <nataroro5@gmail.com>
2023-08-25 21:41:50 +0600

    removed links from the image

--- src/App.tsx
+++ src/App.tsx
@@ -20,79 +20,79 @@ type SectionRefs = {
 function App() {
 
   const user = {
-      email: "dev.nessi@gmail.com",
-      linkedin: "https://www.linkedin.com/in/nataliakn/",
-      github: "https://github.com/Abrosss",
-      work: [
-        {
-          type: "fullstack",
-          projects: [
-            {
-              title: "Verb Conjugator",
-              img: ProjectImage1,
-              description: "A full-stack web application for searching, translating and conjugating spanish verbs. API can be used by anyone for searching verbs. An instruction can be found on the website's main page.",
-              type: "fullstack",
-              live: "https://spanish-verbs.netlify.app/",
-              stacks: ["React", "Node.js", "Scss", "API"],
-              github: "https://github.com/Abrosss/spanish_verbs",
-              demo: ProjectVideo
-            },
-      
-            {
-              title: "Slideshow Maker",
-              img: ProjectImage2,
-              description: "A full-stack app which allows users to create art collections and play slideshows. Full functionality is presented in the demo below.",
-              type: "fullstack",
-              live: "https://slidery.netlify.app/profile",
-              stacks: ["React", "Node.js", "Scss"],
-              github: "https://github.com/Abrosss/galleria-expanded",
-              demo: ProjectVideo2
-            },
-            {
-              title: "Quiz Builder",
-              img: ProjectImage4,
-              description: "A full-stack web application for making quizzes",
-              live: "https://prepped.dev",
-              stacks: ["React", "Node.js", "Scss"],
-              github: "https://github.com/Abrosss/devquiz-quiz-app",
-              demo: ""
-            },
-            {
-              title: "Scoot",
-              img: ProjectImage3,
-              description: "A multi-page full-stack web application for renting and managing scoot rides. Create an account. Use the app to find the nearest scooter to you. Rent a ride and review your previous rides in the profile. Change the profile settings.",
-              type: "fullstack",
-              live: "https://getscootin.netlify.app/",
-              stacks: ["React", "Node.js", "Scss"],
-              github: "https://github.com/Abrosss/scootin-renting-app",
-              demo: ProjectVideo3
-            }
-          ]
-        },
-        {
-          type: "frontend",
-          projects: [
-            {
-              title: "Coffeeroasters",
-              img: ProjectImage5,
-              description: "A multi-page fully responsive website for building a subscription plan for coffee delivery services. Customize your order frequency, quantity, even your roast style and grind type.",
-              type: "frontend",
-              live: "https://coffee-sub.vercel.app/",
-              stacks: ["Javascript", "SCSS"],
-              github: "https://github.com/Abrosss/coffee-shop",
-              demo: ""
-            },
-            
-          ]
-        }
-      ]
-    
-    }
-      
-    
-  
+    email: "dev.nessi@gmail.com",
+    linkedin: "https://www.linkedin.com/in/nataliakn/",
+    github: "https://github.com/Abrosss",
+    work: [
+      {
+        type: "fullstack",
+        projects: [
+          {
+            title: "Verb Conjugator",
+            img: ProjectImage1,
+            description: "A full-stack web application for searching, translating and conjugating spanish verbs. API can be used by anyone for searching verbs. An instruction can be found on the website's main page.",
+            type: "fullstack",
+            live: "https://spanish-verbs.netlify.app/",
+            stacks: ["React", "Node.js", "Scss", "API"],
+            github: "https://github.com/Abrosss/spanish_verbs",
+            demo: ProjectVideo
+          },
 
-  
+          {
+            title: "Slideshow Maker",
+            img: ProjectImage2,
+            description: "A full-stack app which allows users to create art collections and play slideshows. Full functionality is presented in the demo below.",
+            type: "fullstack",
+            live: "https://slidery.netlify.app/profile",
+            stacks: ["React", "Node.js", "Scss"],
+            github: "https://github.com/Abrosss/galleria-expanded",
+            demo: ProjectVideo2
+          },
+          {
+            title: "Quiz Builder",
+            img: ProjectImage4,
+            description: "A full-stack web application for making quizzes",
+            live: "https://prepped.dev",
+            stacks: ["React", "Node.js", "Scss"],
+            github: "https://github.com/Abrosss/devquiz-quiz-app",
+            demo: ""
+          },
+          {
+            title: "Scoot",
+            img: ProjectImage3,
+            description: "A multi-page full-stack web application for renting and managing scoot rides. Create an account. Use the app to find the nearest scooter to you. Rent a ride and review your previous rides in the profile. Change the profile settings.",
+            type: "fullstack",
+            live: "https://getscootin.netlify.app/",
+            stacks: ["React", "Node.js", "Scss"],
+            github: "https://github.com/Abrosss/scootin-renting-app",
+            demo: ProjectVideo3
+          }
+        ]
+      },
+      {
+        type: "frontend",
+        projects: [
+          {
+            title: "Coffeeroasters",
+            img: ProjectImage5,
+            description: "A multi-page fully responsive website for building a subscription plan for coffee delivery services. Customize your order frequency, quantity, even your roast style and grind type.",
+            type: "frontend",
+            live: "https://coffee-sub.vercel.app/",
+            stacks: ["Javascript", "SCSS"],
+            github: "https://github.com/Abrosss/coffee-shop",
+            demo: ""
+          },
+
+        ]
+      }
+    ]
+
+  }
+
+
+
+
+
   const [block, setBlock] = useState(false)
   const [showVideo, setShowVideo] = useState("")
   const sectionRefs: SectionRefs = {
@@ -103,56 +103,54 @@ function App() {
   };
   const [activeSection, setActiveSection] = useState('#fullstack');
   function getThreshold() {
-     
+
     let windowWidth = window.innerWidth;
-  
+
     if (windowWidth < 1000) {
       return 0.5;
     } else if (windowWidth > 1000) {
       return 0.8;
-    } 
+    }
   }
   useEffect(() => {
-  console.log(getThreshold())
+    console.log(getThreshold())
     const observer = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
         let ratio = entry.intersectionRatio;
 
-        
+
         if (ratio > 0.5) {
           setActiveSection(`#${entry.target.id}`)
         }
-       
+
       })
     }, {
       threshold: getThreshold(),
       root: null,
-    
+
     })
-    
+
     const longobserver = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
         let ratio = entry.intersectionRatio;
-       if(ratio > 0.1) setActiveSection(`#${entry.target.id}`)
+        if (ratio > 0.1) setActiveSection(`#${entry.target.id}`)
       })
     }, {
       threshold: 0.1,
       root: null,
-    
+
     })
-    if(!block) {
+    if (!block) {
       longobserver.observe(sectionRefs['fullstack'].current as HTMLElement)
-      Object.values(sectionRefs).forEach((sectionRef) => 
-  
-      {
-        
-       observer.observe(sectionRef.current as HTMLElement)
-      
+      Object.values(sectionRefs).forEach((sectionRef) => {
+
+        observer.observe(sectionRef.current as HTMLElement)
+
       })
     }
-   
 
-    
+
+
     return () => {
       observer.disconnect(); // Disconnect the observer when the component unmounts
     };
@@ -171,61 +169,61 @@ function App() {
       setBlock(false)
     }, 2000)
   };
- 
-  
+
+
 
 
 
   return (
     <section className='page'>
       {showVideo !== "" &&
-      <section className='video' onClick={() => setShowVideo('')}>
-      <video src={showVideo} controls>
-									
-								  </video>
-      </section>
+        <section className='video' onClick={() => setShowVideo('')}>
+          <video src={showVideo} controls>
+
+          </video>
+        </section>
       }
-      
+
       {window.innerWidth > 1000 &&
         <aside className='sidebar'>
           <div className='flower' >
-          <img src={Flower}></img>
-          </div>
-          
-        <nav>
-          <div className={activeSection === "#fullstack" ? "active" : ""} onClick={() => smoothScroll('#fullstack')}>
-            <span className={activeSection === "#fullstack" ? "active" : ""}>FULL-STACK APPLICATIONS</span>
-          </div>
-          <div className={activeSection === "#frontend" ? "active" : ""} onClick={() => smoothScroll('#frontend')}>
-            <span className={activeSection === "#frontend" ? "active" : ""}>FRONTEND APPLICATIONS</span>
-          </div>
-          <div className={activeSection === "#contact" ? "active" : ""} onClick={() => smoothScroll('#contact')}>
-            <span className={activeSection === "#contact" ? "active" : ""}>CONTACT</span>
+            <img src={Flower}></img>
           </div>
 
-        </nav>
-      </aside>
+          <nav>
+            <div className={activeSection === "#fullstack" ? "active" : ""} onClick={() => smoothScroll('#fullstack')}>
+              <span className={activeSection === "#fullstack" ? "active" : ""}>FULL-STACK APPLICATIONS</span>
+            </div>
+            <div className={activeSection === "#frontend" ? "active" : ""} onClick={() => smoothScroll('#frontend')}>
+              <span className={activeSection === "#frontend" ? "active" : ""}>FRONTEND APPLICATIONS</span>
+            </div>
+            <div className={activeSection === "#contact" ? "active" : ""} onClick={() => smoothScroll('#contact')}>
+              <span className={activeSection === "#contact" ? "active" : ""}>CONTACT</span>
+            </div>
+
+          </nav>
+        </aside>
       }
-    
+
       <section className='main'>
         <header>
           <h1>NATALIA KONDRATYUK</h1>
           <h2>software developer portfolio</h2>
         </header>
         <aside className='sidebar horizontal'>
-        <nav>
-          <div  onClick={() => smoothScroll('#fullstack')}>
-            <span className={activeSection === "#fullstack" ? "active" : ""}>FULL-STACK APPLICATIONS</span>
-          </div>
-          <div  onClick={() => smoothScroll('#frontend')}>
-            <span className={activeSection === "#frontend" ? "active" : ""}>FRONTEND APPLICATIONS</span>
-          </div>
-          <div  onClick={() => smoothScroll('#contact')}>
-            <span className={activeSection === "#contact" ? "active" : ""}>CONTACT</span>
-          </div>
+          <nav>
+            <div onClick={() => smoothScroll('#fullstack')}>
+              <span className={activeSection === "#fullstack" ? "active" : ""}>FULL-STACK APPLICATIONS</span>
+            </div>
+            <div onClick={() => smoothScroll('#frontend')}>
+              <span className={activeSection === "#frontend" ? "active" : ""}>FRONTEND APPLICATIONS</span>
+            </div>
+            <div onClick={() => smoothScroll('#contact')}>
+              <span className={activeSection === "#contact" ? "active" : ""}>CONTACT</span>
+            </div>
 
-        </nav>
-      </aside>
+          </nav>
+        </aside>
         <section ref={sectionRefs['parent']} className='portfolio'>
           {user.work.map(block => (
             <section className='portfolio-block' >
@@ -233,21 +231,14 @@ function App() {
               <section className='portfolio-grid' id={block.type} ref={sectionRefs[block.type]} >
                 {block.projects.map(project => (
                   <div className='box'>
-                    <div className='img-container'>
-                      <div className='links'>
-                        <a target='blank' href={project.live}>LIVE </a>
-                        <a target='blank' href={project.github}>GITHUB    <span className='icon github'></span></a>
-                        {project.demo !== "" &&
-                         <a className='demo' onClick={() => setShowVideo(project.demo)}>WATCH DEMO</a>
-                        }
-                       
-                      </div>
+                    <a className='img-container' href={project.live} target='blank'>
+
 
                       <img className="project-image" src={project.img} alt='project image'></img>
 
 
 
-                    </div>
+                    </a>
                     <div className='tech'>
                       {project.stacks.map((skill, index) => (
                         <>
@@ -263,14 +254,20 @@ function App() {
 
                       ))}
                     </div>
-
-                    <a href={project.live} target='blank' className='project-title'>{project.title}</a>
-
-
-
-
+                    <div className='title-section'>
+                      <a href={project.live} target='blank' className='project-title'>{project.title}</a>
+                      <a target='blank' href={project.github}><span className='icon github'></span></a>
+                    </div>
 
                     <p>{project.description}</p>
+                    <div className='links'>
+
+
+                      {project.demo !== "" &&
+                        <a className='demo' onClick={() => setShowVideo(project.demo)}>WATCH DEMO</a>
+                      }
+
+                    </div>
                   </div>
                 ))}
 
@@ -279,14 +276,14 @@ function App() {
             </section>
           ))}
 
-
-          <section ref={sectionRefs['contact']}   id='contact' className='portfolio-block contact'>
+                    
+          <section ref={sectionRefs['contact']} id='contact' className='portfolio-block contact'>
             <h5>Get in touch</h5>
             <a href={`mailto:${user.email}`}>{user.email}</a>
             <section className='contact-links'>
               <a href={user.github} target='blank'><img className='icon' src={GithubIcon} alt='github icon'></img></a>
               <a href={user.linkedin} target='blank'><img className='icon' src={LinkedIn} alt='linkedin icon'></img></a>
-              
+
             </section>
           </section>
         </section>
